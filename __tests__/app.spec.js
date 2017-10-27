7ffd9a1ca7e25beddfc9509bd3d3bc4ccdf88333
Joseph Chou <joseph98c@gmail.com>
2017-10-27 11:15:36 -0700

    complete lab

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -1,6 +1,29 @@
 'use strict';
 
-import {greet, invertCase} from '../js/app'; //import the functions to test!
+import { greet, invertCase } from '../js/app'; //import the functions to test!
 
 /* tests go here */
 
+describe('Basic math', function () {
+    test('should add numbers correctly', function () {
+        let result = 1 + 1;
+        expect(result).toEqual(2);
+    });
+});
+
+describe('invertCase() function', function () {
+    test('should invert lowercase to uppercase correctly', function () {
+        let inverted = invertCase('thequickbrownfoxjumpsoverthelazydog');
+        expect(inverted).toEqual('THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG');
+    });
+    
+    test('should invert uppercase to lowercase correctly', function () {
+        let inverted = invertCase('THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG');
+        expect(inverted).toEqual('thequickbrownfoxjumpsoverthelazydog');
+    });
+    
+    test('should invert mixes of upper and lowercase correctly', function () {
+        let inverted = invertCase('tHeQuIcKbRoWnFoXjUmPsOvErThElAzYdOg');
+        expect(inverted).toEqual('ThEqUiCkBrOwNfOxJuMpSoVeRtHeLaZyDoG');
+    });
+});
--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -3,5 +3,17 @@
 import * as fs from 'fs'; //import functions for working with file system
 const htmlContent = fs.readFileSync('index.html', 'utf-8'); //load the HTML content
 
-/* Your tests go here */
+// Make libraries available
+const $ = require('jquery');
+window.$ = $;
 
+// Load in javascript
+require('../js/index.js');
+
+/* Your tests go here */
+describe('Panic button', function() {
+    test('should display alert when pressed', function() {
+        $('button').click();
+        expect($('.alert').css("visibility") == "hidden").toEqual(false);
+    });
+});
